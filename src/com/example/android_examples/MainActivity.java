c0d3ecfd4f01a946719a2c9056ba75ff645eabdc
Maximiliano Burgos <maxib2007@gmail.com>
2014-06-23 12:09:11 -0300

    Testing backend and app

--- src/com/example/android_examples/MainActivity.java
+++ src/com/example/android_examples/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.android_examples;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	
+	TextView tResult;
+	Button bSumar, bRestar;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		tResult = (TextView) findViewById(R.id.tResult);
+		bSumar = (Button) findViewById(R.id.bSumar);
+		bRestar = (Button) findViewById(R.id.bRestar);
+		
+		tResult.setText("Probando!");
 	}
 }
