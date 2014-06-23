e0e22acd8aabf91b2566e798553f09ba73c82b47
Maximiliano Burgos <maxib2007@gmail.com>
2014-06-23 13:04:46 -0300

    Add ListActivity

--- gen/com/example/android_examples/R.java
+++ gen/com/example/android_examples/R.java
@@ -30,7 +30,8 @@ public final class R {
         public static final int tResult=0x7f070000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_calculadora=0x7f030000;
+        public static final int activity_main=0x7f030001;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
--- src/com/example/android_examples/MainActivity.java
+++ src/com/example/android_examples/MainActivity.java
@@ -1,41 +1,37 @@
 package com.example.android_examples;
 
 import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
-public class MainActivity extends Activity implements View.OnClickListener {
+public class MainActivity extends ListActivity {
 	
-	TextView tResult;
-	Button bSumar, bRestar;
-	int counter;
-
+	String classes[] = {"Calculadora", "Item1","Item2","Item3","Item4","Item5","Item6"};
+			
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		
-		tResult = (TextView) findViewById(R.id.tResult);
-		bSumar = (Button) findViewById(R.id.bSumar);
-		bRestar = (Button) findViewById(R.id.bRestar);
-		counter = 0;
-		
-		bSumar.setOnClickListener(this);
-		bRestar.setOnClickListener(this);
+		setListAdapter(new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1,classes));
+	}
+
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		// TODO Auto-generated method stub
+		super.onListItemClick(l, v, position, id);
+		String selClass = classes[position];
+		try{
+			Class ourClass = Class.forName("com.example.android_examples." + selClass);
+			Intent ourIntent = new Intent(MainActivity.this, ourClass);
+			startActivity(ourIntent);
+		}catch(ClassNotFoundException e){
+			e.printStackTrace();
+		}
 	}
 	
-	public void onClick(View v)
-	{
-		switch(v.getId()){
-			case R.id.bSumar:
-				counter++;
-				break;
-			case R.id.bRestar:
-				counter--;
-				break;
-		}
-		tResult.setText(String.valueOf(counter));
-	}
 }
