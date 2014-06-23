c33dd8d6b056a0dea0f933835077c7a50b35d0fb
Maximiliano Burgos <maxib2007@gmail.com>
2014-06-23 12:28:08 -0300

    Add calculator example

--- src/com/example/android_examples/MainActivity.java
+++ src/com/example/android_examples/MainActivity.java
@@ -2,13 +2,15 @@ package com.example.android_examples;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
 	
 	TextView tResult;
 	Button bSumar, bRestar;
+	int counter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +20,22 @@ public class MainActivity extends Activity {
 		tResult = (TextView) findViewById(R.id.tResult);
 		bSumar = (Button) findViewById(R.id.bSumar);
 		bRestar = (Button) findViewById(R.id.bRestar);
+		counter = 0;
 		
-		tResult.setText("Probando!");
+		bSumar.setOnClickListener(this);
+		bRestar.setOnClickListener(this);
+	}
+	
+	public void onClick(View v)
+	{
+		switch(v.getId()){
+			case R.id.bSumar:
+				counter++;
+				break;
+			case R.id.bRestar:
+				counter--;
+				break;
+		}
+		tResult.setText(String.valueOf(counter));
 	}
 }
