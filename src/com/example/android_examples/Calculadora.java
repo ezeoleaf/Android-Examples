7a5c013bcfbf910e3e1cc731384417736140a252
Maximiliano Burgos <maxib2007@gmail.com>
2014-06-23 14:41:32 -0300

    Add WeightSum

---
+++ src/com/example/android_examples/Calculadora.java
@@ -0,0 +1,40 @@
+package com.example.android_examples;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Calculadora extends Activity implements View.OnClickListener{
+	TextView tResult;
+	Button bSumar, bRestar;
+	int counter;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_calculadora);
+		
+		tResult = (TextView) findViewById(R.id.tResult);
+		bSumar = (Button) findViewById(R.id.bSumar);
+		bRestar = (Button) findViewById(R.id.bRestar);
+		counter = 0;
+		
+		bSumar.setOnClickListener(this);
+		bRestar.setOnClickListener(this);
+	}
+	
+	public void onClick(View v)
+	{
+		switch(v.getId()){
+			case R.id.bSumar:
+				counter++;
+				break;
+			case R.id.bRestar:
+				counter--;
+				break;
+		}
+		tResult.setText(String.valueOf(counter));
+	}
+}
---
+++ src/com/example/android_examples/WeightSum.java
@@ -0,0 +1,15 @@
+package com.example.android_examples;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WeightSum extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_weight_sum);
+	}
+
+}
