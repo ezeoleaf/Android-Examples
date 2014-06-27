ca1fa2ad3736125953f5485244dd9ad2e6c5af9a
Maximiliano Burgos <maxib2007@gmail.com>
2014-06-27 13:15:39 -0300

    add PowerManager

--- src/com/example/android_examples/GFX.java
+++ src/com/example/android_examples/GFX.java
@@ -1,17 +1,31 @@
 package com.example.android_examples;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
 
 public class GFX extends Activity{
 
 	Maxwell ourView;
+	WakeLock wL;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		PowerManager pM = (PowerManager)getSystemService(Context.POWER_SERVICE);
+		wL = pM.newWakeLock(PowerManager.FULL_WAKE_LOCK, "power");
 		super.onCreate(savedInstanceState);
+		wL.acquire();
 		ourView = new Maxwell(this);
 		setContentView(ourView);
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		wL.release();
 	}
 	
 }
