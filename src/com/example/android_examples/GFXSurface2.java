6fe81220638a64f21e9ea1666498c7d0ce4dae92
Maximiliano Burgos <maxib2007@gmail.com>
2014-06-27 13:03:02 -0300

    Animaciones Avanzadas

--- src/com/example/android_examples/GFXSurface2.java
+++ src/com/example/android_examples/GFXSurface2.java
@@ -15,7 +15,7 @@ import android.view.View.OnTouchListener;
 public class GFXSurface2 extends Activity implements OnTouchListener{
 	
 	MaxwellSurface ourSurfaceView;
-	float x, y, sX, sY, fX, fY;
+	float x, y, sX, sY, fX, fY, dX, dY, aniX, aniY, scaledX, scaledY;
 	Bitmap test, plus;
 
 	@Override
@@ -24,7 +24,7 @@ public class GFXSurface2 extends Activity implements OnTouchListener{
 		super.onCreate(savedInstanceState);
 		ourSurfaceView = new MaxwellSurface(this);
 		ourSurfaceView.setOnTouchListener(this);
-		x = y = sX = sY = fX = fY = 0;
+		x = y = sX = sY = fX = fY = dX = dY = aniX = aniY = scaledX = scaledY = 0;
 		test = BitmapFactory.decodeResource(getResources(), R.drawable.greenball);
 		plus = BitmapFactory.decodeResource(getResources(), R.drawable.redball);
 		setContentView(ourSurfaceView);
@@ -46,9 +46,34 @@ public class GFXSurface2 extends Activity implements OnTouchListener{
 
 	@Override
 	public boolean onTouch(View v, MotionEvent event) {
-		// TODO Auto-generated method stub
+
+		try {
+			Thread.sleep(50);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		x = event.getX();
 		y = event.getY();
+		
+		switch(event.getAction()){
+		case MotionEvent.ACTION_DOWN:
+			sX = event.getX();
+			sY = event.getY();
+			dX = dY = aniX = aniY = scaledX = scaledY = fX = fY = 0;
+			break;
+		case MotionEvent.ACTION_UP:
+			fX = event.getX();
+			fY = event.getY();
+			dX = fX-sX;
+			dY = fY-sY;
+			scaledX = dX/30;
+			scaledY = dY/30;
+			x = y = 0;
+			break;
+		}
+		
 		return true;
 	}
 	
@@ -92,9 +117,18 @@ public class GFXSurface2 extends Activity implements OnTouchListener{
 				Canvas canvas = ourHolder.lockCanvas();
 				canvas.drawRGB(02, 02, 150);
 				if(x != 0 && y != 0){
-					
 					canvas.drawBitmap(test, x-(test.getWidth()/2), y-(test.getHeight()/2), null);
 				}
+				if(sX != 0 && sY != 0){
+					canvas.drawBitmap(plus, sX-(plus.getWidth()/2), sY-(plus.getHeight()/2), null);
+				}
+				if(fX != 0 && fY != 0){
+					canvas.drawBitmap(test, fX-(test.getWidth()/2)-aniX, fY-(test.getHeight()/2)-aniY, null);
+					canvas.drawBitmap(plus, fX-(plus.getWidth()/2), fY-(plus.getHeight()/2), null);
+				}
+				aniX = aniX + scaledX;
+				aniY = aniY + scaledY;
+				
 				ourHolder.unlockCanvasAndPost(canvas);
 			}
 		}
